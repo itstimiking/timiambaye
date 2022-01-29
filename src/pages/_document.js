1a56b552366ef5d5b5cfaf93596da745d5929f08
itstimiking <26443032+itstimiking@users.noreply.github.com>
2022-01-29 15:53:18 +0100

    No change

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -45,7 +45,7 @@ export default class MyDocument extends Document {
           <meta property="fb:app_id" content="0.0.1" />
           
           <meta property="twitter:card" content="summary_large_image" />
-          <meta property="twitter:title" content="I am a fullstack developer with React and react native, Django Node.js and Flask and an android developer using kotlin" />
+          <meta property="twitter:title" content="I develop Fullstack websites with React, Django and Flask and cross platform android and iOS mobile softwares using React Native" />
           <meta property="twitter:description" content="Hello there, Its Timi Ambaye. I am a fullstack website dev and a mobile software developer. Android and iOS" />
           <meta property="twitter:image" content="https://www.timiambaye.com/images/banner.png" />
         </Head>
