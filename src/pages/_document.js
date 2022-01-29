d1d7013fde3e539a80072336d768386f3f61e9e7
itstimiking <26443032+itstimiking@users.noreply.github.com>
2022-01-29 15:38:35 +0100

    No change

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -35,17 +35,17 @@ export default class MyDocument extends Document {
           <meta name="keywords" content="Timi Ambaye, Web, mobile kotlin, full stack React developer, react native." />
           <meta name="content" content="Timi Ambaye potfolio website. Full stack website developer using react, Django, Nodejs, python and also mobile application development with kotlin and react native." />
           <meta name="title" content="Timi Ambaye - Mobile software developer, React, React-native, kotlin" />
-          <meta name="description" content="Hello there, Its Timi Ambaye. I am a react-native and kotlin mobile developer, a fullstack react web developer as well." />
+          <meta name="description" content="Hello there, Its Timi Ambaye. I am a fullstack, react, react-native, and kotlin mobile developer." />
           <meta property="og:type" content="website" />
           <meta property="og:title" content="React Developer, React Native and Kotlin Android developer. Timi Ambaye" />
-          <meta property="og:description" content="Hello there. Its Timi Ambaye I am an Android native and cross platform React and React Native developer" />
+          <meta property="og:description" content="Hello there, I am a Fullstack website dev and a cross platform mobile software developer using React Native" />
           <meta property="og:image" content="https://www.timiambaye.com/images/banner.png" />
           <meta property="og:url" content="https://www.timiambaye.com/" />
           <meta property="fb:app_id" content="0.0.1" />
           
           <meta property="twitter:card" content="summary_large_image" />
           <meta property="twitter:title" content="I am a fullstack developer with React and react native, Django Node.js and Flask and an android developer using kotlin" />
-          <meta property="twitter:description" content="Hello there, Its Timi Ambaye. I am a mobile software developer" />
+          <meta property="twitter:description" content="Hello there, Its Timi Ambaye. I am a fullstack website dev and a mobile software developer. Android and iOS" />
           <meta property="twitter:image" content="https://www.timiambaye.com/images/banner.png" />
         </Head>
         <body>
